c22f3cffb6148a791a821a273e4e4533ca5a19ab
Vadim <_s_e_t__@ukr.net>
2025-03-25 15:47:10 +0200

    add some code

--- controllers/auth.js
+++ controllers/auth.js
@@ -128,22 +128,51 @@ exports.getNewPassword = (req, res, next) => {
   User.findOne({
     resetToken: token,
     resetTokenExpiration: { $gt: Date.now() },
-  }).then(user => {
-    let message = req.flash("error");
-    if (message.length > 0) {
-      message = message[0];
-    } else {
-      message = null;
-    }
-    res.render("auth/new-password", {
-      path: "/new-password",
-      pageTitle: "New Password",
-      errorMessage: message,
-      userId: user._id.toString()
+  })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  }).catch(err => {
-    console.log(err)
-  });
+};
 
-  
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save()
+    }).then(reset => {
+      res.redirect('/login')
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
